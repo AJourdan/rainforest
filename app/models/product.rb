c05573ee4ddb8cc9b240be1af2aea6bc39dd3901
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-12 16:14:50 -0500

    Adding correct associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
+  has_many :reviews
+  has_many :users, through: :reviews
+ end
+
 
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
@@ -8,5 +12,4 @@ class Product < ActiveRecord::Base
   end
 
 
-
-end
+ 
