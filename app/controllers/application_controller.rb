30de6ea91bd9db463cfa3d516766492a25cafb49
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-13 13:30:57 -0500

    Writing review routhes, controller and views and partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    else
+	  return nil
   end
 
   helper_method :current_user
+
+  def authenticate_user
+  	unless current_user
+  		redirect_to '/login'
+  	end
+  end
 
 end
